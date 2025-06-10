0de209c3198e41898cca4dd88b6ad55bd5147146
AliMahmoud26 <AliMahmoudHelal26@gmail.com>
2025-06-10 09:29:40 +0300

    Turning the code into just one page which is app.jsx

--- 1- Accordion Nugget App/src/App.jsx
+++ 1- Accordion Nugget App/src/App.jsx
@@ -1,24 +1,40 @@
-import data from './Data'
-import React, { useState } from 'react';
-import SingleQuestion from './Questions';
+import { useState } from "react";
+import questions from "./Data"
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 function App() {
-  const [questions, setQuestions] = useState(data);
+  const [openId, setOpenId] = useState(null);
 
   return (
-      <main className='container'>
-          <h3>Questions and Answers about login</h3>
-          <section className="info">
-              {
-                  questions.map((question) => {
-                      return (
-                          <SingleQuestion key={question.id} {...question} setQuestions={setQuestions} />
-                      )
-                  })
-              }
-          </section>
-      </main>
-    );
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {
+            questions.map((question) => {
+              const {id, title, info} = question;
+              const isOpen = openId === id;
+
+              return (
+                <article key={id} className="question">
+                  <header>
+                    <h4>{title}</h4>
+                    <button
+                      className="btn"
+                      onClick={() => setOpenId(isOpen ? null : id)}
+                    >
+                      {isOpen ? <AiOutlineMinus /> : <AiOutlinePlus />}
+                    </button>
+                  </header>
+                  {isOpen && <p>{info}</p>}
+                </article>
+              )
+            })
+          }
+        </section>
+      </div>
+    </main>
+  )
 }
 
 export default App
